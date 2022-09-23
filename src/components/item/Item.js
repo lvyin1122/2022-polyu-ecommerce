8e7f8b795d928c118e3d4e7c1dd10602b2ae0415
lvyin1122 <khiwhyzqd@outlook.com>
2022-09-23 10:28:49 +0800

    basic version for cart context

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,35 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Item.css";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import CartContext from "../../store/cart-context";
 
 const Item = (props) => {
-  const [btnClicked, setBtnClicked] = useState(false);
   const price = `$${props.price.toFixed(2)}`;
 
   const cartCtx = useContext(CartContext);
   const addToCartHandler = () => {
-    if (btnClicked) {
-      cartCtx.removeItem(props.id);
-      setBtnClicked(false);
-    } else {
-      cartCtx.addItem({
-        id: props.id,
-        name: props.name,
-        price: props.price,
-        imgSrc: props.imgSrc,
-        author: props.author,
-        amount: 1,
-      });
-      setBtnClicked(true);
-    }
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      price: props.price,
+      imgSrc: props.imgSrc,
+      author: props.author,
+      amount: 1,
+    });
   };
 
   return (
     <div className="item">
       <Link to={`/product/${props.id}`}>
-        <img className="itemImage" src={props.imgSrc} alt=""/>
+        <img className="itemImage" src={props.imgSrc} alt="" />
       </Link>
       <div className="info">
         <Link to={`/product/${props.id}`} className="itemName">
@@ -41,7 +34,7 @@ const Item = (props) => {
             <span className="price">{price}</span>
           </div>
           <button className="infoRight" onClick={addToCartHandler}>
-            {btnClicked ? "Added" : "Add to cart"}
+            Add to cart
           </button>
         </div>
       </div>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,61 +1,39 @@
 import CartContext from "./cart-context";
-import { useReducer } from "react";
+import { useState } from "react";
 
 // Define initial state for cart items
 const initialCartState = {
   items: [],
-}
-
-// Define reducer for different action on shopping cart
-// The reducer will execute custom logic to the state according to the action type
-const cartReducer = (state, action) => {
-  switch (action.type) {
-    case "ADD":
-      return {
-        ...state,
-        items: state.items.filter((item) => item.id !== action.item.id).concat(action.item),
-      };
-    case "REMOVE":
-      return {
-        ...state,
-        items: state.items.filter((item) => item.id !== action.id),
-      };
-    case "CLEAR":
-      return {
-        ...state,
-        items: initialCartState,
-      }
-    default:
-      return state;
-  }
 };
 
 // Define CartProvider component
 const CartProvider = (props) => {
-  // Define current cart state and dispatch function with useReducer
-  const [cartState, dispatchCartAction] = useReducer(
-    cartReducer,
-    initialCartState
-  );
+  // Apply useState hook to update the state of the cart
+  const [cartState, setCart] = useState(initialCartState);
 
-  // The handler function will pass the action to the reducer function
+  // Define the behaviour of the three handler with setCart()
   const addItemToCartHandler = (item) => {
-    dispatchCartAction({ type: "ADD", item: item});
+    setCart(
+      (prevCart) => ({
+      items: prevCart.items.concat(item),
+    }));
   };
 
-  const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({ type: "REMOVE", id: id });
+  const removeItemHandler = (id) => {
+    setCart((prevCart) => ({
+      items: prevCart.items.filter((item) => item.id !== id),
+    }));
   };
 
   const clearCartHandler = () => {
-    dispatchCartAction({ type: "CLEAR" });
+    setCart(initialCartState);
   };
 
-  // Create cart context and pass it with the provider
+  // Create cart context and with the item state and functions
   const cartContext = {
     items: cartState.items,
     addItem: addItemToCartHandler,
-    removeItem: removeItemFromCartHandler,
+    removeItem: removeItemHandler,
     clearCart: clearCartHandler,
   };
 
